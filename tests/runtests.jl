e3c74fe754ccb6d64d7b0ac30adad974aae8c6bd
Luis Manuel Oliveira <loliveira042@pt.ema.ad.pwcinternal.com>
2020-01-30 17:23:41 +0000

    Changed Types and methods naming to match styling guidelines, added docstrings

---
+++ tests/runtests.jl
@@ -0,0 +1,37 @@
+using HTTP
+using Sockets
+using JSON
+include("../src/VueJS.jl")
+using Revise
+using .VueJS
+# include("../src/HtmlElement.jl")
+# include("../src/VueElement.jl")
+# include("../src/VueComponent.jl")
+# include("../src/binding.jl")
+# include("../src/data.jl")
+# include("../src/base.jl")
+# include("../src/grid.jl")
+# include("../src/page.jl")
+
+
+router = HTTP.Router()
+
+dom = HtmlElement("tag", Dict("class"=>"test-class"), "valor")
+input = HtmlElement("h4", Dict("type"=>"text", "placeholder"=>"luis"), "texto")
+
+function index(req)
+    myvue = VueElement("luis", "h4", value="texto", placeholder="luis", type="text")
+    comp = VueComponent("luis", [myvue])
+    return page([myvue])
+    #return JSON.json(Dict("welcome"=>"home"))
+end
+
+HTTP.@register(router, "GET", "/", index)
+
+@async HTTP.serve(router, "127.0.0.1" , 9000)
+
+function b(; kwargs...)
+    args=Dict(string(k)=>v for (k,v) in kwargs)
+    println(args)
+end
+b(b=2)

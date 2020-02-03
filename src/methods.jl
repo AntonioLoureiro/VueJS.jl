199ea994994b3d2cff8427e5fa5c54f0fc1e596e
Luis Manuel Oliveira <loliveira042@pt.ema.ad.pwcinternal.com>
2020-02-03 12:05:03 +0000

    updated to reflect new developments; updated docstrings, HtmlElement calls and edited calls to HtmlElement constructors

---
+++ src/methods.jl
@@ -0,0 +1,19 @@
+function methods_script(c::VueStruct)
+    out=[]
+    for (f_name,script) in c.methods
+
+        str="""$f_name :(function(event) {
+        for (key of Object.keys(app_state)) {
+        eval("var "+key+" = app_state."+key)
+        };
+
+        return  function(event) {
+          $script
+        };
+        })()
+        """
+        push!(out,str)
+
+    end
+    return "methods:{$(join(out,","))}"
+end

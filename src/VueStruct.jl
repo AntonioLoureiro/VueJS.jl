522947ffa066b8629f5436fdec2f0f5f991a0b39
Luis Manuel Oliveira <loliveira042@pt.ema.ad.pwcinternal.com>
2020-02-07 15:26:17 +0000

    fix VueStruct merge issues

--- src/VueStruct.jl
+++ src/VueStruct.jl
@@ -1,37 +1,15 @@
 mutable struct EventHandler
-    
+
     kind::String
     id::String
     args::Vector{String}
     script::String
     function_script::String
-    
+
 end
 
 mutable struct VueStruct
 
-<<<<<<< HEAD
-    id::String
-    grid::Array
-    binds::Dict{String,Any}
-    cols::Union{Nothing,Int64}
-    data::Dict{String,Any}
-    def_data::Dict{String,Any}
-    methods::Dict{String,Any}
-    computed::Dict{String, Any}
-    watched::Dict{String, Any}
-end
-
-function VueStruct(
-    id::String,
-    garr::Array;
-    binds=Dict{String,Any}(),
-    data=Dict{String,Any}(),
-    methods=Dict{String,Any}(),
-    computed=Dict{String, Any}(),
-    watched=Dict{String, Any}(),
-    kwargs...)
-=======
      id::String
      grid::Array
      binds::Dict{String,Any}
@@ -43,24 +21,17 @@ function VueStruct(
 end
 
 function VueStruct(id::String,garr::Array;binds=Dict{String,Any}(),data=Dict{String,Any}(),methods=Dict{String,Any}(),computed=Dict{String,Any}(),watched=Dict{String,Any}(),kwargs...)
->>>>>>> 64041f72472cf14607659b5a5d11128483770872
 
     args=Dict(string(k)=>v for (k,v) in kwargs)
 
     haskey(args,"cols") ? cols=args["cols"] : cols=nothing
-    
+
     events=create_events((methods=methods,computed=computed,watched=watched))
-    
+
     scope=[]
-<<<<<<< HEAD
-    garr=element_path(garr, scope)
-    comp=VueStruct(id,garr,trf_binds(binds),cols,data,Dict{String,Any}(),methods, computed, watched)
-    element_binds!(comp, binds=comp.binds)
-=======
     garr=element_path(garr,scope)
     comp=VueStruct(id,garr,VueJS.trf_binds(binds),cols,data,Dict{String,Any}(),events)
     element_binds!(comp,binds=comp.binds)
->>>>>>> 64041f72472cf14607659b5a5d11128483770872
     update_data!(comp,data)
 
     ## Cols
@@ -80,7 +51,7 @@ function element_path(arr::Array,scope::Array)
     for (i,rorig) in enumerate(new_arr)
         r=deepcopy(rorig)
         ## Vue Element
-        if typeof(r) <: VueElement
+        if typeof(r)==VueElement
 
             new_arr[i].path=scope_str
 
@@ -93,14 +64,14 @@ function element_path(arr::Array,scope::Array)
             new_arr[i].grid=element_path(r.grid,scope2)
             new_binds=Dict{String,Any}()
             for (k,v) in new_arr[i].binds
-               for (ki,vi) in v
+               for (kk,vv) in v
                     path=scope2_str=="" ? k : scope2_str*"."*k
-                    values=Dict(path=>ki)
-                    for (kij,vij) in vi
-                        if haskey(new_binds,kij)
-                            new_binds[kij][vij]=values
+                    values=Dict(path=>kk)
+                    for (kkk,vvv) in vv
+                        if haskey(new_binds,kkk)
+                            new_binds[kkk][vvv]=values
                         else
-                            new_binds[kij]=Dict(vij=>values)
+                            new_binds[kkk]=Dict(vvv=>values)
                         end
                     end
                 end
@@ -116,7 +87,7 @@ function element_path(arr::Array,scope::Array)
 end
 
 function dom_scripts(el::VueStruct)
-    
+
     scripts=[]
     push!(scripts,"const app_state = $(vue_json(el.def_data))")
 
@@ -137,5 +108,5 @@ function dom_scripts(el::VueStruct)
                  HtmlElement("v-container",Dict("fluid"=>true),arr_dom)))
 
     return (dom=dom,scripts=scripts)
-    
+
 end

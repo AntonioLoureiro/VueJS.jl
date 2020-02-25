0738664bc2bf846860bf48ae86757e05b11e8f59
Antonio Loureiro <ajose.loureiro@gmail.com>
2020-02-25 17:32:34 +0000

    added vue2editor

--- src/Core/Base.jl
+++ src/Core/Base.jl
@@ -5,7 +5,7 @@ HEAD=HtmlElement("head",
     ])
 
     #Production scripts INCLUDE_SCRIPTS=["https://cdn.jsdelivr.net/npm/vue@2.x/dist/vue.min.js","https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.js"]
-    INCLUDE_SCRIPTS=["https://cdn.jsdelivr.net/npm/vue@2.x/dist/vue.js","https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js"]
+    INCLUDE_SCRIPTS=["https://cdn.jsdelivr.net/npm/vue@2.x/dist/vue.js","https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js","https://cdn.jsdelivr.net/npm/tiptap-vuetify"]
     INCLUDE_STYLES=["https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900","https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css",
     "https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css"]
 

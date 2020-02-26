ea871037cd15e1de17d97e31a37f80eaf3f1f613
Luis Manuel Oliveira <loliveira042@pt.ema.ad.pwcinternal.com>
2020-02-26 16:28:42 +0000

    added support for submission as a VueElement's event
    moved js closure to separate variable; added xhr std_events; altered submit method logic to allow submission of specified struct elements

--- src/Core/Events.jl
+++ src/Core/Events.jl
@@ -24,11 +24,17 @@ function create_events(events::NamedTuple)
     return hs
 end
 
+js_closure = function(;scope::String="@scope@")
+    script=""" for (key of Object.keys($scope)) {
+        eval("var "+key+" = $scope."+key)
+    };"""
+    return script
+end
 
 function_script!(eh::EventHandler)=nothing
 
 function function_script!(eh::CustomEventHandler)
-        
+
         if eh.path==""
             scope="app_state"
         else
@@ -38,17 +44,12 @@ function function_script!(eh::CustomEventHandler)
         args = size(eh.args, 1) > 0 ? join(eh.args, ",") : "event"
 
         str="""$(eh.id) :(function($args) {
-        for (key of Object.keys(@scope@)) {
-        eval("var "+key+" = @scope@."+key)
-        };
-
+            $(js_closure(scope=scope))
         return  function($args) {
           $(eh.script)
         };
         })()
         """
-
-    str=replace(str,"@scope@"=>scope)
 
     eh.function_script=str
 
@@ -81,32 +82,85 @@ function get_json_attr(d::Dict,a::String,path="app_state")
     return out
 end
 
+function std_events!(vs::VueStruct, new_es::Vector{EventHandler})
 
-function std_events!(vs::VueStruct,new_es::Vector{VueJS.EventHandler})
-    
+    ##xhr
+    function_script = """xhr : function(contents, url=window.location.pathname, method="POST", async=true, success="", error="") {
+
+    console.log(contents)
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            if (this.status == 200) {
+                success != "" ? success : console.log(this.responseText);
+            } else {
+                error != "" ? error : console.log('Status: ' + this.status + ' ' + this.statusText + ' : ' + this.responseText);
+            }
+        }
+    }
+    xhr.open(method, url, async);
+    xhr.send(contents);
+    }
+    """
+    push!(new_es,StdEventHandler("methods","xhr","",function_script))
+
     ## Submit Method
-    value_script=replace(JSON.json(VueJS.get_json_attr(vs.def_data,"value")),"\""=>"")
-    function_script="""submit : function(context) {
+    value_script=replace(JSON.json(get_json_attr(vs.def_data,"value")),"\""=>"")
+    function_script="""submit : function(context, url, method, async, success, error) {
         var ret=$value_script
-        console.log(ret)
-        }"""
-    
-    push!(new_es,VueJS.StdEventHandler("methods","submit","",function_script))
-    
+        $(js_closure(scope="app"))
+        if (context && context.length > 0) {
+            out = {}
+            for (key in ret) {
+                if (context.includes(key)) {
+                    out[key] = ret[key]
+                }
+            }
+            ret = out
+        }
+        return xhr(JSON.stringify(ret), url, method, async, success, error)
+    }"""
+    push!(new_es,StdEventHandler("methods","submit","",function_script))
+
     ## Open Method
     function_script="""open : function(url,name) {
         name = typeof name !== 'undefined' ? name : '_self';
         window.open(url,name);
         }"""
-    
-    push!(new_es,VueJS.StdEventHandler("methods","open","",function_script))
-    
+
+    push!(new_es,StdEventHandler("methods","open","",function_script))
+
     ## Datatable Col Render
     function_script="""datatable_col_render : function(item,render_script) {
         return render_script(item)
         }"""
-    
-    push!(new_es,VueJS.StdEventHandler("methods","datatable_col_render","",function_script))
-    
+
+    push!(new_es,StdEventHandler("methods","datatable_col_render","",function_script))
+
     return nothing
+end
+
+"""
+Wrapper around submit and xhr method(s)
+Allows submissions to be defined at VueElement level as an action, `onclick`, `onchange`, etc
+### Examples
+```julia
+@el(lun,"v-text-field",value="Luanda",label="Luanda",disabled=false)
+@el(opo,"v-text-field",value="Oporto",label="Oporto")
+@el(sub, "v-btn", value="Submit All", click=submit("api", context=[lun, opo],
+    success="this.window.alert('teste');", error="this.console.log('teste');"))
+```
+"""
+function submit(
+    url::String;
+    method::String="POST",
+    async::Bool=true,
+    success::String="",
+    error::String="",
+    context::Union{Nothing, Vector}=nothing,
+    )
+    success = success != "" ? "(function() {$success})()" : "\'$success\'"
+    error = error != "" ? "(function() {$error})()" : "\'$error\'"
+    contents = context != nothing ? [x.id for x in context] : []
+    return "submit($(contents != [] ? replace(JSON.json(contents), "\""=>"'") : "null"), '$url', '$method', $async, $success, $error)"
 end

4bf30232c9391a0aa183ea7456470964dec3826f
AntonioLoureiro <ajose.loureiro@gmail.com>
2020-03-16 10:06:31 +0000

    Update VuetifyBase.jl

--- src/Vuetify/VuetifyBase.jl
+++ src/Vuetify/VuetifyBase.jl
@@ -34,6 +34,7 @@ UPDATE_VALIDATION["v-data-table"]=(x)->begin
                     ## Default Renders
                     if !haskey(x.attrs,"col_format") || (haskey(x.attrs,"col_format") && !haskey(x.attrs["col_format"],n))
                         digits=maximum(skipmissing(df[:,Symbol(n)]))>=1000 ? 0 : 2
+			eltype(df[!,i])<:Union{Missing,Int} ? digits=0 : nothing
                         haskey(x.attrs,"col_format") ? nothing : x.attrs["col_format"]=Dict{String,Any}()
                         x.attrs["col_format"][n]="x=> x==null ? x : x.toLocaleString('pt',{minimumFractionDigits: $digits, maximumFractionDigits: $digits})"
                     end

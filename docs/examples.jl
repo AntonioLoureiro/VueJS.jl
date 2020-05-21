43a3288106fdaadf89332443106df2f97499fcc1
Antonio Loureiro <ajose.loureiro@gmail.com>
2020-05-21 19:16:46 +0000

    new Example

--- docs/examples.jl
+++ docs/examples.jl
@@ -1,11 +1,11 @@
-examples=["Elements_Basics"=>"""
+examples=["Elements Basics"=>"""
 @el(r1,"v-text-field",value="R1 Value",label="R1") # Default cols=2 using a 12 cols grid
 @el(r2,"v-text-field",value="R2 Value",label="R2",disabled=true,cols=6)
 @el(r3,"v-text-field",value="R3 Value",label="R3",disabled=false,cols=3)
 tx=html("p","Text",Dict("align"=>"center")) # You can use normal html Elements with all the attributes, default cols=2
 page([r1,r2,r3,tx])
 """,
-"Basic_Grid"=>"""
+"Basic Grid"=>"""
 @el(r1,"v-text-field",value="R1 Value",label="R1",disabled=true)
 @el(r2,"v-text-field",value="R2 Value",label="R2",disabled=false)
 @el(r3,"v-text-field",value="R3 Value",label="R3",disabled=false)
@@ -15,7 +15,7 @@ page([r1,r2,r3,tx])
 @el(r7,"v-slider",value=20,label="Slider 7")
 page([r1,r2,[r3,r4,r5],r6,r7])
 """,
-"Basic_Binding"=>"""
+"Basic Binding"=>"""
 @el(slider,"v-slider",value=20,label="Use Slider",cols=4)
 @el(sel,"v-select",items=["red","green","blue"],label="Select Color",value="red")
 @el(r1,"v-text-field",label="R1",binds=Dict("value"=>"slider.value","label"=>"r2.value")) ## Binding of element attributes to other elements attributes
@@ -24,7 +24,7 @@ page([r1,r2,[r3,r4,r5],r6,r7])
 tx=html("h2","{{slider.value}}") ## You can use curly brace sintax to point plain html to element attributes
 page([slider,sel,[r1,r2,chip,tx]])
 """,
-"Basic_Events"=>"""
+"Basic Events"=>"""
 @el(slider,"v-slider",value=200,min=0,max=1000,label="Use Slider",cols=4)
 @el(chip,"v-chip",text-color="white",x-large=true,binds=Dict("content"=>"slider.value","color"=>"slider.value >500 ? 'red' : 'blue'"))
 @el(chip2,"v-chip",text-color="white",color="green",content="<v-icon>mdi-mouse</v-icon>",x-large=true,mouseover="slider.value=0;alert.content='RESET with Hover';alert.value=true")
@@ -36,7 +36,7 @@ page([slider,sel,[r1,r2,chip,tx]])
 @el(btn_open,"v-btn",content="OPEN",text-color="white",click="open(sel.value)")
 page([alert,slider,[btn_add,btn_add_100,btn_toggle_reset],[chip,chip2],[sel,btn_open]])
 """,
-"Basic_Datatables"=>"""
+"Basic Datatables"=>"""
 df=DataFrame()
 df[!,:Class]=rand(["A","B","C"],10)
 df[!,:Text]=rand(["ABC","DEF","GHI"],10)
@@ -53,5 +53,16 @@ df[!,:Action]=df[!,:Text]
 @el(btn,"v-btn",content="{{item.Text}}",binds=Dict("color"=>"item.Value<0 ? 'red' : 'blue'"),click="alert.content=item.Text;alert.value=true")
 @el(d3,"v-data-table",items=df,col_template=Dict("Action"=>btn),cols=3)
 
-page([[[st,d1],spacer(),[sel,d2],spacer(),[spacer(rows=4),d3,alert]]])"""
+page([[[st,d1],spacer(),[sel,d2],spacer(),[spacer(rows=4),d3,alert]]])""",
+"Navigation and Bar"=>"""    
+@el(navel,"v-navigation-drawer",expand-on-hover=false,items=[Dict("icon"=>"mdi-apple","title"=>"Apple","href"=>"https://www.apple.com"),
+                                                            Dict("icon"=>"mdi-airplane-takeoff","title"=>"Flights","href"=>"https://www.skyscanner.com"),
+                                                            Dict("icon"=>"mdi-cart-outline","title"=>"Shopping","href"=>"https://www.amazon.com")])
+
+@el(homeb,"v-btn",icon=true,value="<v-icon>mdi-home</v-icon>",click="open('/home')")
+@el(searchb,"v-btn",icon=true,value="<v-icon>mdi-magnify</v-icon>",click="open('https://google.com')")
+barel=bar([homeb,"APP",spacer(),searchb]);
+
+@el(icon_btn,"v-btn",content="Material Design Icons Page",click="open('https://materialdesignicons.com')")
+page([icon_btn],navigation=navel,bar=barel)"""
 ]

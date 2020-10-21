fb0bcf7181eeae54df1b36edcc5eeaad23da5e18
Antonio Loureiro <ajose.loureiro@gmail.com>
2020-10-21 16:44:18 +0000

    fix nested file input

--- src/Core/Parsing.jl
+++ src/Core/Parsing.jl
@@ -1,6 +1,6 @@
 function parse(req::HTTP.Request)
     
-    body=Dict()
+     body=Dict()
     headers=Dict{String,String}(req.headers)
     content_type=get(headers,"Content-Type","")
     args=HTTP.URIs.queryparams(HTTP.URI(req.target))
@@ -16,17 +16,33 @@ function parse(req::HTTP.Request)
             end
         end
 
+        nl=nested_levels(body)
         for f in files_arr            
-            name_arr=String.(split(f["name"],"."))[2:end-2]
+            name_arr=String.(split(f["name"],"."))
+            ln=length(name_arr)
+            st=ln-(nl+1)
+            st<2 ? st=2 : nothing
+            name_arr=name_arr[st:ln-2]
             obj=body
             for n in name_arr
                 obj=obj[n]
             end 
             push!(obj,f)
         end
+        
     else
        body=JSON.parse(String(req.body))
     end
     
     return (body=body,headers=headers,args=args)
+end
+
+function nested_levels(body::Dict;n=1)
+    for (k,v) in body
+        if v isa Dict
+            ret=nested_levels(v;n=n+1)
+            ret>n ? n=ret : nothing
+        end
+    end
+    return n
 end

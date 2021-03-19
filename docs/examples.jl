ae3ea64eb607cef176d116de2eb6345ed6811057
Luís Oliveira <11480189+charlieIT@users.noreply.github.com>
2021-03-19 16:32:26 +0000

    Added menu example
    Added menu example that uses both custom `val` property and page methods

--- docs/examples.jl
+++ docs/examples.jl
@@ -45,7 +45,15 @@ items=[Dict("title"=>"Action 1","href"=>"https://www.google.com"),Dict("title"=>
 @el(m1,"v-btn",menu=items,value="MENU 1")
 @el(menu,"v-menu",items=items,dark=true)
 @el(m2,"v-btn",menu=menu,value="MENU 2")
-page([[ch1,ch2,m1,m2]])    
+
+side_actions_def = [
+        Dict("title"=>"Query1", "val"=>Dict("query"=>"Query1",  "what"=>"www.google.com")),
+        Dict("title"=>"Query2", "val"=>Dict("query"=>"Query2",  "what"=>"www.google.com"))]
+
+@el(actions_menu, "v-menu", items=side_actions_def)
+@el(side_menu,    "v-btn", content="<v-icon>mdi-dots-vertical</v-icon>", icon=true, menu=actions_menu, click="query_method(item.val.what, item.val.query)")
+page([[ch1,ch2,m1,m2, side_menu]], 
+      methods=Dict("query_method"=>" function(what, query) {open('http://' + what + '/search?q=' + query, '_blank') }")) 
 """,
 "Basic Datatables"=>"""
 df=DataFrame()

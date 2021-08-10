101f986dbd6690ab6c0c2f00235b53a92ccc62d9
Luís Oliveira <cljljlj@gmail.com>
2021-08-10 13:51:02 +0000

    altered examples logic
    title pages and names now point to files under docs/examples docs/notebooks
    
    live code examples now moved to a binary file instead of created as string for @evaluation

---
+++ docs/examples/basic_datatables.jl
@@ -0,0 +1,17 @@
+df=DataFrame()
+df[!,:Class]=rand(["A","B","C"],10)
+df[!,:Text]=rand(["ABC","DEF","GHI"],10)
+df[!,:Value]=rand(10).*10000 .-5000
+
+@el(st,"v-text-field",label="Search")
+@el(d1,"v-data-table",items=df,binds=Dict("search"=>"st.value"),cols=3)
+
+@el(sel,"v-select",label="Filter Class",items=["","A","B","C"],change="filter_dt(d2,'Class',sel.value)")
+@el(d2,"v-data-table",items=df,filter=Dict("Class"=>"=="),cols=3)
+
+df[!,:Action]=df[!,:Text]
+@el(alert,"v-alert",type="success",text=true,cols=3)
+@el(btn,"v-btn",content="{{item.Text}}",binds=Dict("color"=>"item.Value<0 ? 'red' : 'blue'"),click="alert.content=item.Text;alert.value=true")
+@el(d3,"v-data-table",items=df,col_template=Dict("Action"=>btn),cols=3)
+
+page([[[st,d1],spacer(),[sel,d2],spacer(),[spacer(rows=4),d3,alert]]])

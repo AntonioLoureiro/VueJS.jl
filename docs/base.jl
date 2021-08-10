f25d7a51dc8ac6af91a998a02fdcb26072e72526
Luís Oliveira <11480189+charlieIT@users.noreply.github.com>
2021-08-10 15:03:56 +0000

    added repo name to default url

--- docs/base.jl
+++ docs/base.jl
@@ -2,8 +2,12 @@
 EXAMPLES_DIR    = joinpath(@__DIR__, "examples")
 NOTEBOOKS_DIR   = joinpath(@__DIR__, "notebooks")
 
+# Repository name
+REPO_NAME       = "VueJS.jl"
+
 # Relative web locations
-DOCS_PATH       = "/docs"
+BASE_PATH       = "/$REPO_NAME"
+DOCS_PATH       = joinpath(BASE_PATH, "docs")
 NOTEBOOKS_PATH  = joinpath(DOCS_PATH, "notebooks")
 TARGET_DIR      = "public"
 INDEX_PAGE      = "index.html"

c5b983c14fb3872dd6383730d8f89a4ea45fddf3
Luís Oliveira <cljljlj@gmail.com>
2021-08-10 13:52:08 +0000

    moved constants and deployment values to a separate file

---
+++ docs/base.jl
@@ -0,0 +1,12 @@
+# File system locations
+EXAMPLES_DIR    = joinpath(@__DIR__, "examples")
+NOTEBOOKS_DIR   = joinpath(@__DIR__, "notebooks")
+
+# Base Github pages url 
+BASE_URL        = "https://antonioloureiro.github.io/VueJS.jl"
+
+# Relative web locations
+DOCS_PATH       = "docs"
+NOTEBOOKS_PATH  = joinpath(DOCS_PATH, "notebooks")
+TARGET_DIR      = "public"
+INDEX_PAGE      = "components.html"

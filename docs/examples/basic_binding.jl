2346084050948795a20e9549a66c87e6314b432c
AntonioLoureiro <ajose.loureiro@gmail.com>
2024-08-30 16:25:42 +0100

    Update basic_binding.jl

--- docs/examples/basic_binding.jl
+++ docs/examples/basic_binding.jl
@@ -1,7 +1,11 @@
-@el(slider,"v-slider",value=20,label="Use Slider",step=1,cols=4)
-@el(sel,"v-select",items=["red","green","blue"],label="Select Color",value="red")
+@el(sel1,"v-select",items=["red","green","blue"],label="Main Color",value="red")
+@el(sel2,"v-select",items=["red","green","blue"],label="Secondary Color",value="blue")
+@el(slider,"v-slider",value=20,label="Use Slider",thumb-label="always",step=1,cols=4)
 @el(r1,"v-text-field",label="R1",binds=Dict("value"=>"slider.value","label"=>"r2.value")) ## Binding of element attributes to other elements attributes
-@el(r2,"v-text-field",value="Label of R1, enter text",label="R2",rules=["v => v.length >= 8 || 'Min 8 characters'"])
-@el(chip,"v-chip",text-color="white",binds=Dict("content"=>"slider.value","color"=>"sel.value"))
+@el(r2,"v-text-field",value="Label of R1, enter text",label="R2",rules=[js"v => v.length >= 8 || 'Min 8 characters'"])
+@el(chip,"v-chip",text-color="white",binds=Dict("content"=>"slider.value","color"=>"sel1.value"))
 tx=html("h2","{{slider.value}}") ## You can use curly brace sintax to point plain html to element attributes
-page([slider,sel,[r1,r2,chip,tx]])
+@el(slider2,"v-slider",value=20,label="Comparative Slider",thumb-label="always",step=1,cols=4)
+@el(r3,"v-text-field",label="R3",value=40.0,v-number=Dict("separator"=>" ","precision"=>2)) # Numbers handled through vue-number-format directive
+@el(chip2,"v-chip",text-color="white",cols=2,content="Comparative Chip",binds=Dict("color"=>"slider2.value>r3.value ? sel1.value : sel2.value"))
+page([[sel1,sel2],slider,[r1,r2,chip,tx],slider2,[r3,chip2]])

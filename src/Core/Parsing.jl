8ab0b0b8b1fd8388d9b33810d7ccd48e7978bcfd
Filipe Lourenço <46935681+filipemlourenco@users.noreply.github.com>
2026-02-04 18:52:14 +0000

    Update Parsing.jl

--- src/Core/Parsing.jl
+++ src/Core/Parsing.jl
@@ -31,7 +31,7 @@ function parse(req::HTTP.Request)
         end
         
     else
-       body=JSON.parse(String(req.body), dicttype = Dict)
+       body=JSON.parse(String(req.body), Dict)
     end
     
     return (body=body,headers=headers,args=args)
